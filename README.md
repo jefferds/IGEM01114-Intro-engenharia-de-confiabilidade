805904468efd7fc9c7be4b39c7e5045f8057aa47
jefferds <jefferds@gmail.com>
2021-09-13 15:49:12 +0000

    Deploying to gh-pages from  @ 6c8f04990a324aafeb8e70c1e56ea7e9b7d7d6d4 🚀

---
+++ README.md
@@ -0,0 +1,3 @@
+# Linguagens de Programação
+
+Repositório com material de disciplina de Introdução à Engenharia de Confiabilidade.
---
+++ Unidade 01 - Conceituação de confiabilidade.jl
@@ -0,0 +1,60 @@
+### A Pluto.jl notebook ###
+# v0.16.0
+
+using Markdown
+using InteractiveUtils
+
+# ╔═╡ e9af4550-b632-11eb-1d0d-e11b45468706
+html"<button onclick=present()> Presente </button>"
+
+# ╔═╡ 4fb54a13-3db2-4475-bd49-fd0f9d5cefa4
+md"""
+## Plano Unidade 01 - Conceituação de confiabilidade
+
+### Tema relacionado na ementa:
+
+1. Conceituação de confiabilidade
+
+- 1.2. Evolução histórica da confiabilidade e suas principais áreas de aplicação
+- 1.3. Qualidade e confiabilidade
+- 1.4. Principais conceitos associados à confiabilidade
+- 1.5. Gestão da confiabilidade
+
+**Fonte:**  Flávio Sanson Folgliatto e José Luis Ribeiro Duarte. _**Confiabilidade e Manutenção Industrial**_. Rio de Janeiro: Elsevier: ABEPRO, 2011.
+
+**Fonte:**  EBELING, Charles E. _**An introduction to reliability and maintainability engineering**_. Tata McGraw-Hill Education, 2004.
+
+### Resultados de aprendizagem esperados
+
+✔ Apresentar os conceitos de confiabilidade.
+
+✔ Desenvolver estratégias para solução de problemas. 
+
+✔ Estabelecer relações interpessoais e trabalhar em equipe, comunicar-se oralmente e ter responsabilidade coletiva.
+"""
+
+# ╔═╡ 64cb0563-2802-459e-8b14-2430546554ab
+md"""
+### Roteiro
+
+🕐 1. Indicação do tema da atividade a ser executada por cada grupo *(até 13/set/2021)*,
+ 
+- _Conceituação de confiabilidade_;
+
+**Fonte:**  Flávio Sanson Folgliatto e José Luis Ribeiro Duarte. _**Confiabilidade e Manutenção Industrial**_. Rio de Janeiro: Elsevier: ABEPRO, 2011.
+
+🕑 2. Ler os subitens 1.1, à 1.5, e seus subsubitens. Postar no Classroom resumo de até uma página do que estudou *(até 13/set/2021)*;
+
+🕔 3. Seleção dos subtemas com justificativa para o que será aprendendido e experienciado durante a unidade e cada estudante elabora os estudos dos subtemas (modelos, algoritmos, códigos, casos, simulações e análises) *(até 14/set/2021)*;
+
+🕙 4. Apresentações do subtemas *(até 20/set/2021)*;
+
+🕑 5. Avaliações (sala ou extra sala) *(até 21/set/2021)* e
+
+🕒 6. Feedback (sala) *(até 21/set/2021)*.
+"""
+
+# ╔═╡ Cell order:
+# ╟─e9af4550-b632-11eb-1d0d-e11b45468706
+# ╟─4fb54a13-3db2-4475-bd49-fd0f9d5cefa4
+# ╟─64cb0563-2802-459e-8b14-2430546554ab
