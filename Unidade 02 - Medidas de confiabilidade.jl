1d5499ec9175636d29bbbb772254eac65d4edf27
jefferds <jefferds@gmail.com>
2021-09-21 14:04:44 -0300

    Unidade 02

---
+++ Unidade 02 - Medidas de confiabilidade.jl
@@ -0,0 +1,57 @@
+### A Pluto.jl notebook ###
+# v0.15.1
+
+using Markdown
+using InteractiveUtils
+
+# ╔═╡ e9af4550-b632-11eb-1d0d-e11b45468706
+html"<button onclick=present()> Presente </button>"
+
+# ╔═╡ 4fb54a13-3db2-4475-bd49-fd0f9d5cefa4
+md"""
+## Plano Unidade 02 - Conceituação de confiabilidade
+
+### Tema relacionado na ementa:
+
+1. Conceituação de confiabilidade
+
+- Medidas de Confiabilidade.
+
+**Fonte:**  Flávio Sanson Folgliatto e José Luis Ribeiro Duarte. _**Confiabilidade e Manutenção Industrial**_. Rio de Janeiro: Elsevier: ABEPRO, 2011.
+
+**Fonte:**  EBELING, Charles E. _**An introduction to reliability and maintainability engineering**_. Tata McGraw-Hill Education, 2004.
+
+### Resultados de aprendizagem esperados
+
+✔ Apresentar os conceitos de confiabilidade.
+
+✔ Desenvolver estratégias para solução de problemas. 
+
+✔ Estabelecer relações interpessoais e trabalhar em equipe, comunicar-se oralmente e ter responsabilidade coletiva.
+"""
+
+# ╔═╡ 64cb0563-2802-459e-8b14-2430546554ab
+md"""
+### Roteiro
+
+🕐 1. Indicação do tema da atividade a ser executada por cada grupo *(até 21/set/2021)*,
+ 
+- _Medidas de Confiabilidade_;
+
+**Fonte:**  Flávio Sanson Folgliatto e José Luis Ribeiro Duarte. _**Confiabilidade e Manutenção Industrial**_. Rio de Janeiro: Elsevier: ABEPRO, 2011.
+
+🕑 2. Ler ites do capítulo 1.6 do Livro do Fogliato e seus subsubitens. Postar no Classroom resumo de até uma página do que estudou *(até 27/set/2021)*;
+
+🕔 3. Seleção dos subtemas com justificativa para o que será aprendendido e experienciado durante a unidade e cada estudante elabora os estudos dos subtemas (modelos, algoritmos, códigos, casos, simulações e análises) *(até 28/set/2021)*;
+
+🕙 4. Apresentações do subtemas *(até 04/0ut/2021)*;
+
+🕑 5. Avaliações ($(html"<a href='https://docs.google.com/forms/d/e/1FAIpQLSep8IcQXjUs96Vm3bEEQ2LNs0vrvKq7ajYplkgNkUiMF8ksIQ/viewform?usp=sf_link' target='_blank'>Formulário Eletrônico no Google Forms (Acesse aqui)</a>")) *(até 05/out/2021)* e
+
+🕒 6. Feedback (sala) *(até 05/out/2021)*.
+"""
+
+# ╔═╡ Cell order:
+# ╟─e9af4550-b632-11eb-1d0d-e11b45468706
+# ╟─4fb54a13-3db2-4475-bd49-fd0f9d5cefa4
+# ╟─64cb0563-2802-459e-8b14-2430546554ab
