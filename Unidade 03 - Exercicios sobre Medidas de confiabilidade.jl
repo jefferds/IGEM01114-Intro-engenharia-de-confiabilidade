e8552f47d551f6a3f1b043453dd356ee9e2639ba
jefferds <jefferds@gmail.com>
2021-10-18 16:40:36 -0300

    Unidade 03

---
+++ Unidade 03 - Exercicios sobre Medidas de confiabilidade.jl
@@ -0,0 +1,64 @@
+### A Pluto.jl notebook ###
+# v0.16.0
+
+using Markdown
+using InteractiveUtils
+
+# ╔═╡ e9af4550-b632-11eb-1d0d-e11b45468706
+html"<button onclick=present()> Presente </button>"
+
+# ╔═╡ 4fb54a13-3db2-4475-bd49-fd0f9d5cefa4
+md"""
+## Plano Unidade 03 - Exercicios sobre Medidas de confiabilidade
+
+### Tema relacionado na ementa:
+
+1. Conceituação de confiabilidade
+
+- Medidas de Confiabilidade.
+
+**Fonte:**  Flávio Sanson Folgliatto e José Luis Ribeiro Duarte. _**Confiabilidade e Manutenção Industrial**_. Rio de Janeiro: Elsevier: ABEPRO, 2011.
+
+**Fonte:**  EBELING, Charles E. _**An introduction to reliability and maintainability engineering**_. Tata McGraw-Hill Education, 2004.
+
+### Resultados de aprendizagem esperados
+✔ Utilização do vscode e linguagem julia para solução de problema de confibilidade.
+
+✔ Apresentar os conceitos de confiabilidade.
+
+✔ Desenvolver estratégias para solução de problemas. 
+
+✔ Estabelecer relações interpessoais e trabalhar em equipe, comunicar-se oralmente e ter responsabilidade coletiva.
+"""
+
+# ╔═╡ 64cb0563-2802-459e-8b14-2430546554ab
+md"""
+### Roteiro
+
+🕐 1. Indicação do tema da atividade a ser executada por cada grupo *(até 18/out/2021)*,
+ 
+- _Solução de exercícios dos livros sobre medidades de confiabilidade_;
+
+**Fonte:**  Flávio Sanson Folgliatto e José Luis Ribeiro Duarte. _**Confiabilidade e Manutenção Industrial**_. Rio de Janeiro: Elsevier: ABEPRO, 2011.
+
+**Fonte:**  EBELING, Charles E. _**An introduction to reliability and maintainability engineering**_. Tata McGraw-Hill Education, 2004.
+
+🕑 2. Fazer busca rede e postar no Classroom resumo de até uma página sobre o programa vscode e sobre a linguagem julia *(até 19/out/2021)*;
+
+- [https://code.visualstudio.com/](https://code.visualstudio.com/)
+
+- [https://julialang.org/](https://julialang.org/)
+
+🕔 3. Seleção por cada componente do grupo de um exercícios a ser resolvido nas listas disponibilizadas no material da unidade no Classroom. Resolver o exercício para apresentação utilizando vscode e julia. (modelos, algoritmos, códigos, casos, simulações e análises) *(até 19/out/2021)*;
+
+🕙 4. Apresentações dos exercícios *(até 25/0ut/2021)*;
+
+🕑 5. Avaliações ($(html"<a href='https://docs.google.com/forms/d/e/1FAIpQLSep8IcQXjUs96Vm3bEEQ2LNs0vrvKq7ajYplkgNkUiMF8ksIQ/viewform?usp=sf_link' target='_blank'>Formulário Eletrônico no Google Forms (Acesse aqui)</a>")) *(até 26/out/2021)* e
+
+🕒 6. Feedback (sala) *(até 26/out/2021)*.
+"""
+
+# ╔═╡ Cell order:
+# ╟─e9af4550-b632-11eb-1d0d-e11b45468706
+# ╠═4fb54a13-3db2-4475-bd49-fd0f9d5cefa4
+# ╠═64cb0563-2802-459e-8b14-2430546554ab
